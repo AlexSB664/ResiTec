5755305df14ce3c90feb10bd12deb77cd7ff056a
miguel reyes <miguel.angel.reyes122@hotmail.com>
2018-11-23 15:44:25 -0800

    n

--- Administrador/Administradorprincipal.Designer.cs
+++ Administrador/Administradorprincipal.Designer.cs
@@ -39,14 +39,22 @@
             this.proyectosresidencia = new System.Windows.Forms.Button();
             this.datosdealumno = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
-            this.Noproyectob = new System.Windows.Forms.TextBox();
             this.Buscar = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.TextBox();
             this.Nocontrolb = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.Nombre = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Noproyectob = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -54,18 +62,20 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(152, 63);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1153, 344);
+            this.dataGridView1.Size = new System.Drawing.Size(1188, 349);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(1157, 12);
+            this.button1.Location = new System.Drawing.Point(1040, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 45);
+            this.button1.Size = new System.Drawing.Size(148, 64);
             this.button1.TabIndex = 2;
             this.button1.Text = "Status";
             this.button1.UseVisualStyleBackColor = false;
@@ -76,7 +86,7 @@
             this.button2.BackColor = System.Drawing.Color.RoyalBlue;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(12, 59);
+            this.button2.Location = new System.Drawing.Point(12, 63);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -89,7 +99,7 @@
             this.button3.BackColor = System.Drawing.Color.RoyalBlue;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(12, 97);
+            this.button3.Location = new System.Drawing.Point(12, 104);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 36);
             this.button3.TabIndex = 4;
@@ -101,7 +111,7 @@
             this.button5.BackColor = System.Drawing.Color.RoyalBlue;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button5.ForeColor = System.Drawing.Color.White;
-            this.button5.Location = new System.Drawing.Point(12, 148);
+            this.button5.Location = new System.Drawing.Point(12, 160);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 6;
@@ -114,7 +124,7 @@
             this.button6.BackColor = System.Drawing.Color.RoyalBlue;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button6.ForeColor = System.Drawing.Color.White;
-            this.button6.Location = new System.Drawing.Point(12, 187);
+            this.button6.Location = new System.Drawing.Point(12, 198);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 43);
             this.button6.TabIndex = 7;
@@ -126,7 +136,7 @@
             this.button7.BackColor = System.Drawing.Color.RoyalBlue;
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button7.ForeColor = System.Drawing.Color.White;
-            this.button7.Location = new System.Drawing.Point(12, 245);
+            this.button7.Location = new System.Drawing.Point(12, 257);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(75, 43);
             this.button7.TabIndex = 8;
@@ -137,7 +147,7 @@
             // proyectosresidencia
             // 
             this.proyectosresidencia.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.proyectosresidencia.Location = new System.Drawing.Point(152, 413);
+            this.proyectosresidencia.Location = new System.Drawing.Point(3, 3);
             this.proyectosresidencia.Name = "proyectosresidencia";
             this.proyectosresidencia.Size = new System.Drawing.Size(129, 45);
             this.proyectosresidencia.TabIndex = 9;
@@ -149,7 +159,7 @@
             // datosdealumno
             // 
             this.datosdealumno.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.datosdealumno.Location = new System.Drawing.Point(287, 413);
+            this.datosdealumno.Location = new System.Drawing.Point(138, 3);
             this.datosdealumno.Name = "datosdealumno";
             this.datosdealumno.Size = new System.Drawing.Size(129, 45);
             this.datosdealumno.TabIndex = 10;
@@ -161,7 +171,7 @@
             // button8
             // 
             this.button8.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button8.Location = new System.Drawing.Point(422, 413);
+            this.button8.Location = new System.Drawing.Point(273, 3);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(129, 45);
             this.button8.TabIndex = 11;
@@ -170,19 +180,12 @@
             this.button8.UseVisualStyleBackColor = false;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
-            // Noproyectob
-            // 
-            this.Noproyectob.Location = new System.Drawing.Point(164, 25);
-            this.Noproyectob.Name = "Noproyectob";
-            this.Noproyectob.Size = new System.Drawing.Size(147, 20);
-            this.Noproyectob.TabIndex = 12;
-            // 
             // Buscar
             // 
             this.Buscar.BackColor = System.Drawing.Color.DarkGreen;
             this.Buscar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Buscar.ForeColor = System.Drawing.Color.White;
-            this.Buscar.Location = new System.Drawing.Point(12, 10);
+            this.Buscar.Location = new System.Drawing.Point(12, 4);
             this.Buscar.Name = "Buscar";
             this.Buscar.Size = new System.Drawing.Size(75, 43);
             this.Buscar.TabIndex = 13;
@@ -190,18 +193,65 @@
             this.Buscar.UseVisualStyleBackColor = false;
             this.Buscar.Click += new System.EventHandler(this.Buscar_Click);
             // 
-            // label1
+            // panel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(209, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
-            this.label1.TabIndex = 14;
-            this.label1.Text = "No proyecto";
+            this.panel1.Controls.Add(this.Buscar);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(129, 499);
+            this.panel1.TabIndex = 19;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.proyectosresidencia);
+            this.panel2.Controls.Add(this.datosdealumno);
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(129, 413);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1188, 86);
+            this.panel2.TabIndex = 20;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.Noproyectob);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.Nocontrolb);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.Nombre);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(129, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1188, 64);
+            this.panel3.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(429, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Nombre";
+            // 
+            // Nombre
+            // 
+            this.Nombre.Location = new System.Drawing.Point(375, 29);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(147, 20);
+            this.Nombre.TabIndex = 17;
             // 
             // Nocontrolb
             // 
-            this.Nocontrolb.Location = new System.Drawing.Point(335, 25);
+            this.Nocontrolb.Location = new System.Drawing.Point(189, 29);
             this.Nocontrolb.Name = "Nocontrolb";
             this.Nocontrolb.Size = new System.Drawing.Size(147, 20);
             this.Nocontrolb.TabIndex = 15;
@@ -209,27 +259,36 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(373, 9);
+            this.label2.Location = new System.Drawing.Point(234, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 16;
             this.label2.Text = "No control";
             // 
-            // Nombre
+            // label1
             // 
-            this.Nombre.Location = new System.Drawing.Point(519, 25);
-            this.Nombre.Name = "Nombre";
-            this.Nombre.Size = new System.Drawing.Size(147, 20);
-            this.Nombre.TabIndex = 17;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "No proyecto";
             // 
-            // label3
+            // Noproyectob
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(559, 9);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 13);
-            this.label3.TabIndex = 18;
-            this.label3.Text = "Nombre";
+            this.Noproyectob.Location = new System.Drawing.Point(15, 29);
+            this.Noproyectob.Name = "Noproyectob";
+            this.Noproyectob.Size = new System.Drawing.Size(147, 20);
+            this.Noproyectob.TabIndex = 12;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.dataGridView1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(129, 64);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1188, 349);
+            this.panel4.TabIndex = 22;
             // 
             // Administradorprincipal
             // 
@@ -237,30 +296,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(1317, 499);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.Nombre);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.Nocontrolb);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Buscar);
-            this.Controls.Add(this.Noproyectob);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.datosdealumno);
-            this.Controls.Add(this.proyectosresidencia);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Administradorprincipal";
             this.Text = "Panel de control";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Administradorprincipal_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -276,12 +327,16 @@
         private System.Windows.Forms.Button proyectosresidencia;
         private System.Windows.Forms.Button datosdealumno;
         private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.TextBox Noproyectob;
         private System.Windows.Forms.Button Buscar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox Noproyectob;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox Nocontrolb;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox Nombre;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
     }
 }
--- AutomatizacionResidencias/Decorator/EdicionDatosalumno.cs
+++ AutomatizacionResidencias/Decorator/EdicionDatosalumno.cs
@@ -5,10 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using AutomatizacionResidencias;
+using System.Windows.Forms;
+
 namespace AutomatizacionResidencias.Decorator
 {
     public class EdicionDatosalumno : REGISTRO
     {
+        public Alumno alumnodiferencias;
+        public Administrador bi = new Administrador();
+
         public override void Registrardatos(string datos, out string Errores)
         {
             Errores = null;
@@ -50,17 +55,16 @@ namespace AutomatizacionResidencias.Decorator
             {
 
                 var alumnoo = context.Alumno.FirstOrDefault(x => x.NoControl == oldNocontrol);
+                MessageBox.Show(JsonConvert.SerializeObject(alumnoo));
+                alumnodiferencias = Comparar(alumnoo,this.alumno);
+                
                 var alumnoeliminar = context.Alumno.FirstOrDefault(x => x.NoControl == oldNocontrol);
                 var residencia = context.Proyecto_Residencia.FirstOrDefault(x => x.No_Proyecto == this.alumno.NoProyecto);
                 var usuario = context.Usuario.FirstOrDefault(x => x.Usuario1 == alumnoo.Correo);
                 var usuarioeliminar = context.Usuario.FirstOrDefault(x => x.Usuario1 == alumnoo.Correo);
                 context.Alumno.Remove(alumnoeliminar);
                 context.Usuario.Remove(usuario);
-
                 context.SaveChanges();
-              
-
-
                 alumnoo.Nombre = this.alumno.Nombre;
                 alumnoo.Semestre = this.alumno.Semestre;
                 alumnoo.Telefono = this.alumno.Telefono;
@@ -83,8 +87,57 @@ namespace AutomatizacionResidencias.Decorator
                 context.Usuario.Add(usuario);
 
                 context.SaveChanges();
+                MessageBox.Show(JsonConvert.SerializeObject(alumnodiferencias));
 
+            }
+        }
 
+        public Alumno Comparar(Alumno original,Alumno nuevo) {
+            Alumno queencontre = new Alumno();
+
+            if (diferente<int>(original.NoControl,nuevo.NoControl)) {
+                queencontre.NoControl = nuevo.NoControl;
+            }
+            if (diferente<string>(original.Nombre, nuevo.Nombre))
+            {
+                queencontre.Nombre = nuevo.Nombre;
+            }
+            if (diferente<int?>(original.NoProyecto, nuevo.NoProyecto))
+            {
+                queencontre.NoProyecto = nuevo.NoProyecto;
+            }
+            if (diferente<int?>(original.Semestre, nuevo.Semestre))
+            {
+                queencontre.NoControl = nuevo.NoControl;
+            }
+            if (diferente<string>(original.Telefono, nuevo.Telefono))
+            {
+                queencontre.NoControl = nuevo.NoControl;
+            }
+            if (diferente<string>(original.Apellido_Materno, nuevo.Apellido_Materno))
+            {
+                queencontre.NoControl = nuevo.NoControl;
+            }
+            if (diferente<string>(original.Apellido_Paterno, nuevo.Apellido_Paterno))
+            {
+                queencontre.NoControl = nuevo.NoControl;
+            }
+            if (diferente<string>(original.Correo, nuevo.Correo))
+            {
+                queencontre.NoControl = nuevo.NoControl;
+            }
+       
+            return queencontre;
+
+        }
+
+        public bool diferente<T>(T or, T ne) {
+            if (or.Equals(ne))
+            {
+                return false;
+            }
+            else {
+                return true;
             }
         }
     }
